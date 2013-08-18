c5243b8842d7751b9c94ed110d17ef5b45c329ff
Vincent Bernardoff <vb@luminar.eu.org>
2013-08-18 12:46:56 +0100

    Added command line arguments.

--- test/parse_page.ml
+++ test/parse_page.ml
@@ -31,13 +31,13 @@ let decode_page h decoder =
     then Lwt.return None
     else Lwt.return (Some last_id)
 
-let main ?(freq=600.0) ?after ?before ?(limit="100") subreddit =
+let main ?db_uri ?(freq=600.0) ?after ?before ?(limit=100) subreddit =
   (* Creates the "reddit" DB. *)
-  let h = Couchdb.handle () in
+  let h = Couchdb.handle ?uri:db_uri () in
   Couchdb.DB.create h "reddit"
   >>= fun _ ->
   let base_uri = Uri.of_string ("http://www.reddit.com/r/" ^ subreddit ^ "/new.json") in
-  let base_uri = Uri.add_query_param' base_uri ("limit", limit) in
+  let base_uri = Uri.add_query_param' base_uri ("limit", string_of_int limit) in
   let init_uri = match after with
     | None -> base_uri
     | Some id -> Uri.add_query_param' base_uri ("after", id)
@@ -60,11 +60,20 @@ let main ?(freq=600.0) ?after ?before ?(limit="100") subreddit =
       Lwt_unix.sleep freq >>= fun () -> fetch_and_decode uri in
   fetch_and_decode init_uri
 
-let print_usage () = Printf.fprintf stderr "Usage: %s subreddit [limit] [after]\n%!" Sys.argv.(0)
-
 let _ =
-  match Array.length Sys.argv with
-  | 1 -> print_usage ()
-  | 2 -> Lwt_main.run (main Sys.argv.(1))
-  | 3 -> Lwt_main.run (main ~limit:Sys.argv.(2) Sys.argv.(1))
-  | _ -> Lwt_main.run (main ~limit:Sys.argv.(2) ~after:Sys.argv.(3) Sys.argv.(1))
+  let open Arg in
+  let db_uri = ref "http://localhost:5984" in
+  let subreddit = ref "" in
+  let limit = ref 100 in
+  let after = ref None in
+  let freq = ref 600.0 in
+  let speclist = align [
+      "--db_uri", Set_string db_uri, "<string> URI of the CouchDB database in use (default: http://localhost:5984).";
+      "--limit", Set_int limit, "<int> Number of links returned by one API call (default: 100).";
+      "--after", String (fun id -> after := Some id), "<link_id> Get links posted prior <link_id> (default: most recent link).";
+      "--freq", Set_float freq, "<float> Number of seconds between each API call (default: 600)."
+    ] in
+  let anon_fun s = subreddit := s in
+  let usage_msg = "Usage: " ^ Sys.argv.(0) ^ " [--db_uri <string>] [--limit <int>] [--after <link_id>] [--freq <float>] subreddit" in
+  parse speclist anon_fun usage_msg;
+  Lwt_main.run (main ~db_uri:!db_uri ~freq:!freq ?after:!after ~limit:!limit !subreddit)
